b279867ae341f4b7c1ca617ba1111ae96f1caffd
Vitalii Dokalenko <vitaliidokalenko@gmail.com>
2021-01-06 13:55:38 +0100

    refactored TestAppConfig.java to inherit AppConfig.java

--- src/test/java/com/foxminded/university/config/TestAppConfig.java
+++ src/test/java/com/foxminded/university/config/TestAppConfig.java
@@ -1,50 +1,12 @@
 package com.foxminded.university.config;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.jdbc.datasource.init.DataSourceInitializer;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
 
 @Configuration
-@ComponentScan("com.foxminded.university")
+@Import(AppConfig.class)
 @TestPropertySource("database.properties")
 public class TestAppConfig {
 
-	private final static String SCHEMA = "schema.sql";
-
-	@Value("${driver}")
-	private String driver;
-	@Value("${url}")
-	private String url;
-	@Value("${username}")
-	private String username;
-	@Value("${password}")
-	private String password;
-
-	@Bean
-	public DataSource dataSource() {
-		DriverManagerDataSource manager = new DriverManagerDataSource();
-		manager.setDriverClassName(driver);
-		manager.setUrl(url);
-		manager.setUsername(username);
-		manager.setPassword(password);
-		return manager;
-	}
-
-	@Bean
-	public DataSourceInitializer dataSourceInitializer() {
-		ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
-		databasePopulator.addScript(new ClassPathResource(SCHEMA));
-		DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
-		dataSourceInitializer.setDataSource(dataSource());
-		dataSourceInitializer.setDatabasePopulator(databasePopulator);
-		return dataSourceInitializer;
-	}
 }
