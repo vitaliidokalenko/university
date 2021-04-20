b961b684cef5f46b09ef2858003b61c045c3e61c
Vitalii Dokalenko <vitaliidokalenko@gmail.com>
2021-04-21 00:41:23 +0200

    Switch DAO layer to lookup DataSource by JNDI name

--- src/test/java/com/foxminded/university/config/TestAppConfig.java
+++ src/test/java/com/foxminded/university/config/TestAppConfig.java
@@ -1,10 +1,33 @@
 package com.foxminded.university.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 @Import(AppConfig.class)
 public class TestAppConfig {
 
+	@Value("${db.driver}")
+	private String driver;
+	@Value("${db.url}")
+	private String url;
+	@Value("${db.username}")
+	private String username;
+	@Value("${db.password}")
+	private String password;
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource manager = new DriverManagerDataSource();
+		manager.setDriverClassName(driver);
+		manager.setUrl(url);
+		manager.setUsername(username);
+		manager.setPassword(password);
+		return manager;
+	}
 }
