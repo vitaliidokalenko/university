f29d396cfd6ed51c05c1a58ad837b074f4e5ada0
Vitalii Dokalenko <vitaliidokalenko@gmail.com>
2021-05-27 15:48:57 +0200

    converted application to Spring Boot. Revision of dependencies in pom.xml

--- src/main/java/com/foxminded/university/Main.java
+++ src/main/java/com/foxminded/university/Main.java
@@ -1,8 +1,17 @@
 package com.foxminded.university;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(
+		exclude = {
+				DataSourceTransactionManagerAutoConfiguration.class,
+				HibernateJpaAutoConfiguration.class })
 public class Main {
 
 	public static void main(String[] args) {
-
+		SpringApplication.run(Main.class, args);
 	}
 }
