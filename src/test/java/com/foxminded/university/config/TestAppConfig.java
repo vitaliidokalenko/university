3dd74d6416424c492116a29b9fa5e9e78971c453
Vitalii Dokalenko <vitaliidokalenko@gmail.com>
2021-05-30 16:01:02 +0200

    changed *.yml to *.properties in /test due to tests failed in other way

--- src/test/java/com/foxminded/university/config/TestAppConfig.java
+++ src/test/java/com/foxminded/university/config/TestAppConfig.java
@@ -6,12 +6,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 
 @Configuration
 @Import(AppConfig.class)
 @EnableAutoConfiguration
 @ComponentScan("com.foxminded.university.dao")
+@PropertySource("classpath:application.properties")
 public class TestAppConfig {
 
 	@Bean
