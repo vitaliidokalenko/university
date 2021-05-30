fd679e4bd7db9e329864f76d699a9494e164e692
Vitalii Dokalenko <vitaliidokalenko@gmail.com>
2021-05-30 14:50:27 +0200

    added placeholders to set hibernateProperties

--- src/main/java/com/foxminded/university/config/AppConfig.java
+++ src/main/java/com/foxminded/university/config/AppConfig.java
@@ -4,14 +4,25 @@ import java.util.Properties;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
 public class AppConfig {
+
+	@Value("${hibernate.hbm2ddl.auto}")
+	private String hbm2ddlAuto;
+	@Value("${hibernate.current_session_context_class}")
+	private String context;
+	@Value("${hibernate.enable_lazy_load_no_trans}")
+	private String lazyLoad;
 
 	@Bean
 	public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
@@ -31,9 +42,9 @@ public class AppConfig {
 
 	private final Properties hibernateProperties() {
 		Properties hibernateProperties = new Properties();
-		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
-		hibernateProperties.setProperty("current_session_context_class", "thread");
-		hibernateProperties.setProperty("hibernate.enable_lazy_load_no_trans", "true");
+		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", hbm2ddlAuto);
+		hibernateProperties.setProperty("current_session_context_class", context);
+		hibernateProperties.setProperty("hibernate.enable_lazy_load_no_trans", lazyLoad);
 		return hibernateProperties;
 	}
 }
