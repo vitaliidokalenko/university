5c4dffaf7b4dfa9e0f6f7d79a7106ce9fc0d9e6b
Vitalii Dokalenko <vitaliidokalenko@gmail.com>
2021-07-08 16:11:01 +0200

    added handlers for all custom exceptions

--- src/main/java/com/foxminded/university/api/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/com/foxminded/university/api/controller/exception/GlobalExceptionHandler.java
@@ -16,6 +16,20 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.foxminded.university.service.exception.GroupOverflowException;
+import com.foxminded.university.service.exception.IncorrectDurationException;
+import com.foxminded.university.service.exception.NotAvailableGroupException;
+import com.foxminded.university.service.exception.NotAvailableRoomException;
+import com.foxminded.university.service.exception.NotAvailableTeacherException;
+import com.foxminded.university.service.exception.NotCompetentTeacherForCourseException;
+import com.foxminded.university.service.exception.NotEnoughRoomCapacityException;
+import com.foxminded.university.service.exception.NotFoundEntityException;
+import com.foxminded.university.service.exception.NotFoundSubstituteTeacherException;
+import com.foxminded.university.service.exception.NotSuitableRoomForCourseException;
+import com.foxminded.university.service.exception.NotUniqueNameException;
+import com.foxminded.university.service.exception.NotUniqueSequenceException;
+import com.foxminded.university.service.exception.NotWeekDayException;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -37,5 +51,99 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	public void handleRuntimeException(RuntimeException ex, HttpServletResponse response) throws IOException {
 		log.error(ex.getMessage(), ex);
 		response.sendError(HttpStatus.BAD_REQUEST.value());
+	}
+
+	@ExceptionHandler(NotFoundEntityException.class)
+	public void handleNotFoundEntityException(Exception ex, HttpServletResponse response)
+			throws IOException {
+		log.error(ex.getMessage(), ex);
+		response.sendError(HttpStatus.NOT_FOUND.value());
+	}
+
+	@ExceptionHandler(GroupOverflowException.class)
+	public void handleGroupOverflowException(GroupOverflowException ex, HttpServletResponse response)
+			throws IOException {
+		log.error(ex.getMessage(), ex);
+		response.sendError(HttpStatus.CONFLICT.value());
+	}
+
+	@ExceptionHandler(IncorrectDurationException.class)
+	public void handleIncorrectDurationException(IncorrectDurationException ex, HttpServletResponse response)
+			throws IOException {
+		log.error(ex.getMessage(), ex);
+		response.sendError(HttpStatus.CONFLICT.value());
+	}
+
+	@ExceptionHandler(NotAvailableGroupException.class)
+	public void handleNotAvailableExceptions(NotAvailableGroupException ex, HttpServletResponse response)
+			throws IOException {
+		log.error(ex.getMessage(), ex);
+		response.sendError(HttpStatus.CONFLICT.value());
+	}
+
+	@ExceptionHandler(NotAvailableRoomException.class)
+	public void handleNotAvailableRoomException(NotAvailableRoomException ex, HttpServletResponse response)
+			throws IOException {
+		log.error(ex.getMessage(), ex);
+		response.sendError(HttpStatus.CONFLICT.value());
+	}
+
+	@ExceptionHandler(NotAvailableTeacherException.class)
+	public void handleNotAvailableTeacherException(NotAvailableTeacherException ex, HttpServletResponse response)
+			throws IOException {
+		log.error(ex.getMessage(), ex);
+		response.sendError(HttpStatus.CONFLICT.value());
+	}
+
+	@ExceptionHandler(NotCompetentTeacherForCourseException.class)
+	public void handleNotCompetentTeacherForCourseException(NotCompetentTeacherForCourseException ex,
+			HttpServletResponse response)
+			throws IOException {
+		log.error(ex.getMessage(), ex);
+		response.sendError(HttpStatus.CONFLICT.value());
+	}
+
+	@ExceptionHandler(NotEnoughRoomCapacityException.class)
+	public void handleNotEnoughRoomCapacityException(NotEnoughRoomCapacityException ex, HttpServletResponse response)
+			throws IOException {
+		log.error(ex.getMessage(), ex);
+		response.sendError(HttpStatus.CONFLICT.value());
+	}
+
+	@ExceptionHandler(NotFoundSubstituteTeacherException.class)
+	public void handleNotFoundSubstituteTeacherException(NotFoundSubstituteTeacherException ex,
+			HttpServletResponse response)
+			throws IOException {
+		log.error(ex.getMessage(), ex);
+		response.sendError(HttpStatus.NOT_FOUND.value());
+	}
+
+	@ExceptionHandler(NotSuitableRoomForCourseException.class)
+	public void handleNotSuitableRoomForCourseException(NotSuitableRoomForCourseException ex,
+			HttpServletResponse response)
+			throws IOException {
+		log.error(ex.getMessage(), ex);
+		response.sendError(HttpStatus.CONFLICT.value());
+	}
+
+	@ExceptionHandler(NotUniqueNameException.class)
+	public void handleNotUniqueNameException(NotUniqueNameException ex, HttpServletResponse response)
+			throws IOException {
+		log.error(ex.getMessage(), ex);
+		response.sendError(HttpStatus.CONFLICT.value());
+	}
+
+	@ExceptionHandler(NotUniqueSequenceException.class)
+	public void handleNotUniqueSequenceException(NotUniqueSequenceException ex, HttpServletResponse response)
+			throws IOException {
+		log.error(ex.getMessage(), ex);
+		response.sendError(HttpStatus.CONFLICT.value());
+	}
+
+	@ExceptionHandler(NotWeekDayException.class)
+	public void handleNotWeekDayException(NotWeekDayException ex, HttpServletResponse response)
+			throws IOException {
+		log.error(ex.getMessage(), ex);
+		response.sendError(HttpStatus.CONFLICT.value());
 	}
 }
